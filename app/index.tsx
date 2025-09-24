c9f8c3fbc47b6456ea840a3c1a7703eaa6ca2652
Yusuf <teajhaney@gmail.com>
2025-09-24 21:38:22 +0100

    Add clsx dependency and implement CartButton component
    - Added clsx as a dependency in package.json and package-lock.json.
    - Created CartButton component for cart functionality with item count display.
    - Updated global.css to adjust spacing in offer card layout.
    - Refactored app/index.tsx to utilize FlatList for rendering offers and integrated CartButton.

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,87 @@
-import { Text, View } from 'react-native';
+import { CartButton } from '@/components';
+import { images, offers } from '@/constants';
+import cn from 'clsx';
+import { Fragment } from 'react';
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import './global.css';
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl text-center font-bold text-primary font-quicksand-bold">
-        Welcome to Nativewind with expo!
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <FlatList
+        ListHeaderComponent={() => (
+          <View className="flex-between flex-row w-full my-5">
+            <View className="flex-start">
+              <Text className="small-bold text-primary">DELIVER TO</Text>
+              <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+                <Text className="paragraph-bold text-dark-100">Nigeria</Text>
+                <Image
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+
+            <Text>
+              <CartButton />
+            </Text>
+          </View>
+        )}
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
+          return (
+            <View>
+              <Pressable
+                className={cn(
+                  'offer-card',
+                  isEven ? 'flex-row-reverse' : 'flex-row'
+                )}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: '#fffff22' }}
+              >
+                {({ pressed }) => (
+                  <Fragment>
+                    <View className="h-full w-1/2">
+                      <Image
+                        source={item.image}
+                        className="size-full"
+                        resizeMode="contain"
+                      />
+                    </View>
+                    <View
+                      className={cn(
+                        'offer-card_info flex-1',
+                        isEven ? 'pl-10' : 'pr-10'
+                      )}
+                    >
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        resizeMode="contain"
+                        tintColor={'#ffffff'}
+                      />
+                    </View>
+                  </Fragment>
+                )}
+              </Pressable>
+            </View>
+          );
+        }}
+        contentContainerClassName="pb-28 px-5"
+      />
+    </SafeAreaView>
   );
 }
