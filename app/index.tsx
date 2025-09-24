1895bf82c48b65428d93935bd5f74c9b7ef9e134
Yusuf <teajhaney@gmail.com>
2025-09-24 19:34:28 +0100

    Add NativeWind integration and update project structure
    - Added support for NativeWind with a new tailwind.config.js file.
    - Introduced global CSS styles in app/global.css.
    - Updated app.json to include bundler configuration for Metro.
    - Added Babel configuration for NativeWind.
    - Included TypeScript definitions for image imports.
    - Updated package.json and package-lock.json to include new dependencies: nativewind, tailwindcss, and prettier-plugin-tailwindcss.
    - Added custom fonts and various assets including icons and images.
    - Removed unused modal and tab layout components to streamline the project.
    - Updated the main layout and index files to reflect new structure and styles.
    - Added constants for categories and offers to enhance app functionality.

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import './global.css';
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl text-center font-bold text-primary font-quicksand-bold">
+        Welcome to Nativewind with expo!
+      </Text>
+    </View>
+  );
+}
